97e081d3985f18ea561f5c65e0004fda099317d4
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-06-17 07:31:03 -0600

    añado versión con 20 degitos en la contraseña

--- GestorDeContraseñas.py
+++ GestorDeContraseñas.py
@@ -169,7 +169,7 @@ class RegistroApp(tk.Tk):
         self.cargar_datos()
 
     def generar_contrasena(self):
-        longitud = 12
+        longitud = 20
         caracteres = string.ascii_letters + string.digits + string.punctuation
         contrasena = "".join(random.choice(caracteres) for _ in range(longitud))
         self.contrasena_var.set(contrasena)
